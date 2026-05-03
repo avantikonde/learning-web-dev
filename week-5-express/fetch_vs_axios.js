805b5700dbd7f53bb26609f394baaf900cb0e88e
Avanti Komde <avantikonde2020@gmail.com>
2026-05-03 22:45:51 +0530

    Update fetch_vs_axios.js

--- week-5-express/fetch_vs_axios.js
+++ week-5-express/fetch_vs_axios.js
@@ -1,1 +1,37 @@
+//Using fetch 
 
+async function main(){
+    const response = await fetch("https://httpdump.app/dumps/2713620a-701d-4248-b627-4a10337579dc",{
+        method: "GET",
+        username: "Avanti",
+        headers: {
+            "Authorization": "123"
+        }
+    },
+    );
+
+    const textData = await response.text();
+    console.log(textData);
+}
+
+main();
+
+//Using axios
+
+const axios = require("axios");
+
+async function main(){
+    const response = await axios.post("https://httpdump.app/dumps/2713620a-701d-4248-b627-4a10337579dc",
+        {
+            username:"Avanti Konde"
+        },
+        {
+            headers: {
+                "Authorization": "1234"
+            },
+        },
+    );
+    console.log(response.data);
+}
+
+main();
