7d71d08a34282d2fcfdf61251201169adda5d47e
Avanti Konde <avantikonde2020@gmail.com>
2026-05-14 21:48:01 +0530

    Coursera-app
    Set up Express server and define API routes for user, admin, and course.

--- week-8-Project/index.js
+++ week-8-Project/index.js
@@ -1,1 +1,20 @@
-Main backend logic
+const express = require("express");
+const mongoose = require("mongoose");
+
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
+const app = express();
+app.use(express.json());
+
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/course", courseRouter);
+
+async function main() {
+    app.listen(3000);
+    console.log("listening on port 3000")
+}
+
+main()
