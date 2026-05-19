9736b9165b87cf2483c06af194a11a0ab135d6cc
Avanti Konde <avantikonde2020@gmail.com>
2026-05-19 22:10:38 +0530

    User routes with Zod validation and bcrypt

--- week-8-Project/routes/user.js
+++ week-8-Project/routes/user.js
@@ -1,76 +1,74 @@
-const { Router } = require("express");
-const { userModel, purchaseModel, courseModel } = require("../db");
-const jwt = require("jsonwebtoken");
-const  { JWT_USER_PASSWORD } = require("../config");
-const { userMiddleware } = require("../middleware/user");
+const express = require('express');
+const {Router} = require('express');
+const mongoose = require('mongoose');
+const { UserModel , AdminModel , CourseModel , PurchaseModel} = require('./db');
+const jwt = require('jsonwebtoken')
+const {JWT_USER_PASSWORD} = require("../config")
 
 const userRouter = Router();
 
-userRouter.post("/signup", async function(req, res) {
-    const { email, password, firstName, lastName } = req.body; // TODO: adding zod validation
-    // TODO: hash the password so plaintext pw is not stored in the DB
 
-    // TODO: Put inside a try catch block
-    await userModel.create({
-        email: email,
-        password: password,
-        firstName: firstName, 
-        lastName: lastName
-    })
+app.post("/signup", async function(req,res) {
+    const { email, password, firstname, lastname } = req.body;
     
-    res.json({
-        message: "Signup succeeded"
+    const requiredBody = z.object({
+        email: z.string().min(5).max(60).email(),
+        password: z.string().min(5).max(50),
+        firstname: z.string().min(5).max(50),
+        lastname: z.string().min(5).max(50)
     })
+
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if(!parsedDataWithSuccess.success){
+        res.json({
+            message:"Incorrect Format",
+            error: parsedDataWithSuccess.error
+        })
+        return
+    }
+
+    const hashedpassword = await bcrypt.hash(password, 5);
+    console.log(hashedpassword)
+
+
+    await UserModel.create({
+        email: email,
+        password: hashedpassword,
+        firstname: firstname,
+        lastname: lastname
+    })
+
+    req.json({
+        message: "You have signed Up Successfullyy!!"
+    })
+
 })
 
-userRouter.post("/signin",async function(req, res) {
-    const { email, password } = req.body;
+app.post("/signin", async function(req,res) {
+    const { email, password } = req.body;
 
-    // TODO: ideally password should be hashed, and hence you cant compare the user provided password and the database password
-    const user = await userModel.findOne({
+    const user = await UserModel.findOne({
         email: email,
         password: password
-    }); //[]
+    })
 
-    if (user) {
+    const passwordMatch = bcrypt.compare(password, user.password);
+
+    if(user && passwordMatch){
         const token = jwt.sign({
-            id: user._id,
-        }, JWT_USER_PASSWORD);
-
-        // Do cookie logic
-
+            id: user._id
+        }, JWT_SECRET );
         res.json({
             token: token
-        })
+        });
     } else {
         res.status(403).json({
-            message: "Incorrect credentials"
-        })
-    }
-})
-
-userRouter.get("/purchases", userMiddleware, async function(req, res) {
-    const userId = req.userId;
-
-    const purchases = await purchaseModel.find({
-        userId,
-    });
-
-    let purchasedCourseIds = [];
-
-    for (let i = 0; i<purchases.length;i++){ 
-        purchasedCourseIds.push(purchases[i].courseId)
+            message: "Incorrect Credentials"
+        });
     }
 
-    const coursesData = await courseModel.find({
-        _id: { $in: purchasedCourseIds }
-    })
-
-    res.json({
-        purchases,
-        coursesData
-    })
-})
+});
 
 module.exports = {
     userRouter: userRouter
