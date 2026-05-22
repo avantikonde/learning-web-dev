e535e569463167ac0b84ec51398b4ab8933a5b7c
Avanti Konde <avantikonde2020@gmail.com>
2026-05-22 23:39:56 +0530

    counter component with button click
    Add a counter component with state management using useState.

--- week-9-React/counter.jsx
+++ week-9-React/counter.jsx
@@ -1,1 +1,24 @@
+import { useState } from "react";
+
+function App() {
+    const[count, setCount] = React.useState(0)
+    
+    return (
+    <div>
+      <Button count ={count} setCount={setCount}></Button>
+    </div>
+  )
+}
+
+function onButtonClick() {
+    setCount(count + 1);
+  }
+
+  return React.createElement(
+    'button',
+    { onClick: onButtonClick },
+    `Counter ${count}`
+  );
+
+export default App
 
