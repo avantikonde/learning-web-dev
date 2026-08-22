f2fbd841a6894e04e1c81f515ba3e65d3125b41a
Avanti Konde <avantikonde2020@gmail.com>
2026-08-22 23:58:38 +0530

    Add Explicitly typed variables in Explicitly.ts

---
+++ week-14-TypeScript/week-14.2-Interface,types/Explicitly.ts
@@ -0,0 +1,7 @@
+// Explicitly typed variables
+let userName: string = "Avanti";
+let userAge: number = 19;
+let isEmployee: boolean = true;
+
+// TypeScript can also infer types automatically
+let greeting = "Hello Everyone"; 
