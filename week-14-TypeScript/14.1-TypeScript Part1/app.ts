cae8fcc498733feeee80f29a403277de9e197038
Avanti Konde <avantikonde2020@gmail.com>
2026-09-12 23:23:41 +0530

    Create app.ts

---
+++ week-14-TypeScript/14.1-TypeScript Part1/app.ts
@@ -0,0 +1,11 @@
+// A typed variable
+let message: string = "Hello, World!";
+console.log(message);
+
+// A basic function with type annotations
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+let sum: number = addNumbers(10, 15);
+console.log("Sum is: " + sum);
